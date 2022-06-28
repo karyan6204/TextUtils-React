a878deacb0853fd1ef847bd3904cf74f165587bc
Aryan <karyan6204@gmail.com>
2022-06-28 21:13:27 +0530

    fixed some issues in TextUtils

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 // import About from './Components/About';
-// import Alert from './Components/Alert';
+import Alert from './Components/Alert';
 import React,{useState} from 'react'
 import './App.css';
 // import {
@@ -12,26 +12,33 @@ import './App.css';
 
 
 function App() {
-  // const [alert, setAlert]=useState('');
-  // const showAlert = (message,type)=>{
-  //   setAlert({
-  //     msg: message,
-  //     type: type
-  //   })
-  // }
+  const [alert, setAlert]=useState(null);
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
 
   const [mode,setMode]=useState('light')
   const toggleMode = ()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor = '#0e1138';
-      // showAlert('Dark Mode enabled','success')
-      document.title = 'TextUtils - Dark Mode ';
+      showAlert('Dark Mode enabled','success');
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+      // document.title = 'TextUtils - Dark Mode ';
     }else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      // showAlert('Dark Mode enabled','success');
-      document.title = 'TextUtils - Light Mode ';
+      showAlert('Dark Mode disabled','success');
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+      
+      // document.title = 'TextUtils - Light Mode ';
     }
   }
   
@@ -39,14 +46,13 @@ function App() {
     <>
     {/* <Router> */}
     <Navbar title = "TextUtils" mode = {mode} toggleMode = {toggleMode}/>  
-    {/* <Alert alert = {alert}/> */}
+    <Alert alert = {alert}/>
     <div className = "container my-3">
     {/* <Routes>
         <Route exact path="/about" element = {<About/>} />
         <Route exact path="/" element = {<TextForm showAlert = {showAlert} heading = "Enter your Text Below" mode = {mode}/>} />
       </Routes> */}
-      {/* <TextForm showAlert = {showAlert} heading = "Enter your Text Below" mode = {mode}/> */}
-      <TextForm heading = "Enter your Text Below" mode = {mode}/>
+      <TextForm showAlert = {showAlert} heading = "TextUtils - Word Counter, Character Counter, Remove Extra Spaces" mode = {mode}/>
     </div>
     {/* </Router> */}
     </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -5,16 +5,25 @@ export default function TextForm(props) {
     let newText = text.toUpperCase()
     setText(newText)
     props.showAlert('Converted to Upper Case','success')
+    setTimeout(() => {
+      props.showAlert(null);
+    }, 1500);
    }
    const handleLoClick = ()=>{
     let newText = text.toLowerCase()
     setText(newText)
     props.showAlert('Converted to Lower Case','success')
+    setTimeout(() => {
+      props.showAlert(null);
+    }, 1500);
    }
    const handleClearClick = ()=>{
     let newText = ''
     setText(newText)
     props.showAlert('Text has been cleared','success')
+    setTimeout(() => {
+      props.showAlert(null);
+    }, 1500);
    }
    const handleOnChange = (event)=>{
     setText(event.target.value)
@@ -24,11 +33,17 @@ export default function TextForm(props) {
       newText.select();
       navigator.clipboard.writeText(newText.value);
       props.showAlert('Text has been copied','success')
+      setTimeout(() => {
+        props.showAlert(null);
+      }, 1500);
    }
    const handleExtraSpace = ()=>{
       let newText = text.split(/[ ]+/);
       setText(newText.join(" "));
       props.showAlert('Extra Space has been removed','success')
+      setTimeout(() => {
+        props.showAlert(null);
+      }, 1500);
    }
    const [text,setText]=useState('');
     return (
@@ -36,19 +51,19 @@ export default function TextForm(props) {
         <div  style = {{color:props.mode==='dark'?'white':'dark'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value = {text} onChange = {handleOnChange} style = {{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'dark'}} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value = {text} onChange = {handleOnChange} style = {{backgroundColor:props.mode==='dark'?'#07071b':'white',color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className = "btn btn-primary mx-1" onClick={handleUpClick}> Convert to UpperCase </button>
-        <button className = "btn btn-primary mx-1" onClick={handleLoClick}> Convert to LowerCase </button>
-        <button className = "btn btn-primary mx-1" onClick={handleClearClick}> Clear Text </button>
-        <button className = "btn btn-primary mx-1" onClick={handleCopyClick}> Copy Text </button>
-        <button className = "btn btn-primary mx-1" onClick={handleExtraSpace}> Remove Extra space </button>
+        <button className = "btn btn-primary mx-1 my-1" disabled = {text.length===0} onClick={handleUpClick}> Convert to UpperCase </button>
+        <button className = "btn btn-primary mx-1 my-1" disabled = {text.length===0} onClick={handleLoClick}> Convert to LowerCase </button>
+        <button className = "btn btn-primary mx-1 my-1" disabled = {text.length===0} onClick={handleClearClick}> Clear Text </button>
+        <button className = "btn btn-primary mx-1 my-1" disabled = {text.length===0} onClick={handleCopyClick}> Copy Text </button>
+        <button className = "btn btn-primary mx-1 my-1" disabled = {text.length===0} onClick={handleExtraSpace}> Remove Extra space </button>
       </div>
       <br></br>
       <div style = {{color:props.mode==='dark'?'white':'dark'}}>
         <h2>Your Summary</h2>
-        <p>Your Paragraph contains {text.split(" ").length} words and {text.length} characters.</p>
-        <p>It will take {0.008*text.split(" ").length} minutes to read this whole paragraph.</p>
+        <p>Your Paragraph contains {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters.</p>
+        <p>It will take {0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read this whole paragraph.</p>
       </div>
       <div style = {{color:props.mode==='dark'?'white':'dark'}}>
         <h2>Preview</h2>
